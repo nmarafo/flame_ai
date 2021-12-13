f8f2335b9c7b30b61e7839064cc48d09dbafbc4f
norberto <normaafo@gmail>
2021-12-13 12:42:46 +0000

    update VectorAI with limit

--- lib/utils/vector_ai.dart
+++ lib/utils/vector_ai.dart
@@ -1,7 +1,21 @@
 import 'package:flame/extensions.dart';
-import 'package:flame_ai/steer/steerable.dart';
 
 class VectorAI extends Vector2{
 
   VectorAI(): super.zero();
+
+  VectorAI limit(double value){
+    if (value == 0.0) {
+      setZero();
+    } else {
+      var l = length;
+      if (l == 0.0) {
+        return this;
+      }
+      l = value / l;
+      storage[0] *= l;
+      storage[1] *= l;
+    }
+    return this;
+  }
 }
