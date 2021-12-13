878abc629c767a6b33ae899a85458cffca8b0d61
norberto <normaafo@gmail>
2021-12-13 19:43:36 +0000

    configure example without ai

--- example/lib/basic_animation.dart
+++ example/lib/basic_animation.dart
@@ -3,44 +3,23 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
+import 'package:flame_ai_example/commons/ember.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:flutter/services.dart';
 
-class BasicAnimationsExample extends FlameGame with TapDetector {
-  static const description = '''
-    Basic example of `SpriteAnimation`s use in Flame's `FlameGame`\n\n
-    
-    The snippet shows how an animation can be loaded and added to the game
-    ```
-    class MyGame extends FlameGame {
-      @override
-      Future<void> onLoad() async {
-        final animation = await loadSpriteAnimation(
-          'animations/chopper.png',
-          SpriteAnimationData.sequenced(
-            amount: 4,
-            textureSize: Vector2.all(48),
-            stepTime: 0.15,
-          ),
-        );
-    
-        final animationComponent = SpriteAnimationComponent(
-          animation: animation,
-          size: Vector2.all(100.0),
-        );
-    
-        add(animationComponent);
-      }
-    }
-    ```
-    On this example, click or touch anywhere on the screen to dynamically add
-    animations.
-  ''';
+class BasicAnimationsExample extends FlameGame with TapDetector,KeyboardEvents {
+  static const int speed = 200;
 
   late Image creature;
+  late final Ember ember;
+
+  final Vector2 velocity = Vector2(0, 0);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     creature = await images.load('animations/creature.png');
+    ember = Ember(position: size / 2, size: Vector2.all(100));
 
     final animation = await loadSpriteAnimation(
       'animations/chopper.png',
@@ -68,7 +47,15 @@ class BasicAnimationsExample extends FlameGame with TapDetector {
 
     add(animationComponent);
     add(reversedAnimationComponent);
+    add(ember);
     //add(Ember()..position = size / 2);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    final displacement = velocity * (speed * dt);
+    ember.position.add(displacement);
   }
 
   void addAnimation(Vector2 position) {
@@ -84,7 +71,7 @@ class BasicAnimationsExample extends FlameGame with TapDetector {
         loop: false,
       ),
       size: size,
-      removeOnFinish: true,
+      removeOnFinish: false,
     );
 
     animationComponent.position = position - size / 2;
@@ -94,5 +81,25 @@ class BasicAnimationsExample extends FlameGame with TapDetector {
   @override
   void onTapDown(TapDownInfo info) {
     addAnimation(info.eventPosition.game);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      RawKeyEvent event,
+      Set<LogicalKeyboardKey> keysPressed,
+      ) {
+    final isKeyDown = event is RawKeyDownEvent;
+
+    if (event.logicalKey == LogicalKeyboardKey.keyA) {
+      velocity.x = isKeyDown ? -1 : 0;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyD) {
+      velocity.x = isKeyDown ? 1 : 0;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyW) {
+      velocity.y = isKeyDown ? -1 : 0;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyS) {
+      velocity.y = isKeyDown ? 1 : 0;
+    }
+
+    return super.onKeyEvent(event, keysPressed);
   }
 }
